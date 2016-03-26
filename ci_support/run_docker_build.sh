fde54c7715acdfd03e827b16245d0de6499eba2e
ocefpaf <ocefpaf@gmail.com>
2016-03-26 15:35:29 -0300

    Testing pep0440 version.

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -50,58 +50,58 @@ conda info
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
-
+    
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-
-
+    
+    
 
     set -x
     export CONDA_NPY=19
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
-
+    
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-
-
+    
+    
 
     set -x
     export CONDA_NPY=110
     export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
-
+    
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-
-
+    
+    
 
     set -x
     export CONDA_NPY=19
     export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
-
+    
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-
-
+    
+    
 
     set -x
     export CONDA_NPY=110
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
-
+    
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-
-
+    
+    
 
     set -x
     export CONDA_NPY=19
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
-
+    
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-
-
+    
+    
 EOF
