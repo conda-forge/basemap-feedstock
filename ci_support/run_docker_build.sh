44fd553b27f8ec4057e25b17cc90e5518fc96e60
ocefpaf <ocefpaf@gmail.com>
2016-03-26 10:07:05 -0300

    pin conda-build to 1.18.2

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -37,7 +37,7 @@ echo "$config" > ~/.condarc
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
 conda clean --lock
 
-conda update --yes --all
+conda install --yes conda-build==1.18.2
 
 conda info
 
@@ -50,58 +50,58 @@ conda info
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
-    
+
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-    
-    
+
+
 
     set -x
     export CONDA_NPY=19
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
-    
+
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-    
-    
+
+
 
     set -x
     export CONDA_NPY=110
     export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
-    
+
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-    
-    
+
+
 
     set -x
     export CONDA_NPY=19
     export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
-    
+
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-    
-    
+
+
 
     set -x
     export CONDA_NPY=110
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
-    
+
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-    
-    
+
+
 
     set -x
     export CONDA_NPY=19
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
-    
+
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
-    
-    
+
+
 EOF
