6ba1f4b7afee0b42bef268aa653f78c272a0c1ab
Scott Bailey <sbailey@anaconda.com>
2022-02-10 15:11:14 -0700

    typo correction

--- recipe/bld-basemap.bat
+++ recipe/bld-basemap.bat
@@ -6,9 +6,9 @@ ECHO ""
 set GEOS_DIR=%LIBRARY_PREFIX%
 
 :: Jump to the correct build section
-if "%PKG_NAME"=="basemap" GOTO CASE_basemap
-if "%PKG_NAME"=="basemap-data" GOTO CASE_basemap_data
-if "%PKG_NAME"=="basemap-data-hires" GOTO CASE_basemap_data_hires
+if "%PKG_NAME%"=="basemap" GOTO CASE_basemap
+if "%PKG_NAME%"=="basemap-data" GOTO CASE_basemap_data
+if "%PKG_NAME%"=="basemap-data-hires" GOTO CASE_basemap_data_hires
 
 ECHO "Unknown package: %PKG_NAME%"
 EXIT 1
