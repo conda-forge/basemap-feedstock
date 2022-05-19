724697a67306368c15eee3b68c8a8f1556d2fbfa
ReimarBauer <rb.proj@gmail.com>
2022-05-19 20:16:12 +0200

    MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.05.19.14.17.20

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -44,7 +44,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y cython
+/usr/bin/sudo -n yum install -y Cython
 
 
 # make the build number clobber
