271d7178ed60faa258f36a600d56e9e4f9243d8d
Víctor Molina García <molinav@users.noreply.github.com>
2022-05-19 23:10:15 +0200

    Revert changes to resemble feedstock from AnacondaRecipes

--- recipe/bld-basemap.bat
+++ recipe/bld-basemap.bat
@@ -2,6 +2,9 @@ ECHO "==========================================================================
 ECHO "Building %PKG_NAME%"
 ECHO ""
 
+:: Ensure our geos will be used.
+set GEOS_DIR=%LIBRARY_PREFIX%
+
 :: Jump to the correct build section
 if "%PKG_NAME%"=="basemap" GOTO CASE_basemap
 if "%PKG_NAME%"=="basemap-data" GOTO CASE_basemap_data
@@ -12,17 +15,17 @@ EXIT 1
 
 
 :CASE_basemap
-  python -m pip install packages\basemap --no-deps --ignore-installed  -vvv
+  "%PYTHON%" -m pip install packages\basemap --no-deps --ignore-installed  -vvv
   IF errorlevel 1 EXIT 1
   GOTO CASE_end
 
 :CASE_basemap_data
-  python -m pip install packages\basemap_data --no-deps --ignore-installed  -vvv
+  "%PYTHON%" -m pip install packages\basemap_data --no-deps --ignore-installed  -vvv
   IF errorlevel 1 EXIT 1
   GOTO CASE_end
 
 :CASE_basemap_data_hires
-  python -m pip install packages\basemap_data_hires --no-deps --ignore-installed  -vvv
+  "%PYTHON%" -m pip install packages\basemap_data_hires --no-deps --ignore-installed  -vvv
   IF errorlevel 1 EXIT 1
   GOTO CASE_end
 
--- recipe/build-basemap.sh
+++ recipe/build-basemap.sh
@@ -4,19 +4,22 @@ echo "==========================================================================
 echo "Building $PKG_NAME"
 echo ""
 
+# Ensure our geos will be used.
+export GEOS_DIR=$PREFIX
+
 case $PKG_NAME in
 
     basemap)
         rm -f packages/basemap/pyproject.toml
-        python -m pip install packages/basemap --no-deps --ignore-installed -vvv
+        $PYTHON -m pip install packages/basemap --no-deps --ignore-installed -vvv
         ;;
 
     basemap-data)
-        python -m pip install packages/basemap_data --no-deps --ignore-installed -vvv
+        $PYTHON -m pip install packages/basemap_data --no-deps --ignore-installed -vvv
         ;;
 
     basemap-data-hires)
-        python -m pip install packages/basemap_data_hires --no-deps --ignore-installed -vvv
+        $PYTHON -m pip install packages/basemap_data_hires --no-deps --ignore-installed -vvv
         ;;
 
     *)
