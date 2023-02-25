eade3d3a73c7b6dec3b378e25461139d4c7d6db8
Víctor Molina García <molinav@users.noreply.github.com>
2023-02-25 20:10:05 +0100

    Add patch to set proper python binary if cross-compiling

--- recipe/build-basemap.sh
+++ recipe/build-basemap.sh
@@ -10,10 +10,16 @@ rm -f packages/basemap/pyproject.toml
 # Ensure our GEOS library will be used.
 export GEOS_DIR=${PREFIX}
 
-# Select based on the three `basemap-split` packages.
+# Proceed based on the `basemap-split` subpackages.
 case ${PKG_NAME} in
     basemap|basemap-data|basemap-data-hires)
         pkgdir="packages/$(echo ${PKG_NAME} | tr - _)"
+        # Redefine python binary if cross-compiling.
+        if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
+            mkdir -p ${BUILD_PREFIX}/bin
+            rm -f ${BUILD_PREFIX}/bin/python
+            ln -sf ${PREFIX}/bin/python ${BUILD_PREFIX}/bin/python
+        fi
         ${PYTHON} -m pip install -vvv --ignore-installed --no-deps ${pkgdir}
         ;;
     *)
