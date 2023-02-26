be6209c38b48f2fcc1749edf68eed1e5c58b386a
Víctor Molina García <molinav@users.noreply.github.com>
2023-02-26 11:18:50 +0100

    Try defining PYTHON variable when cross-compiling noarch packages

--- recipe/build-basemap.sh
+++ recipe/build-basemap.sh
@@ -10,13 +10,18 @@ rm -f packages/basemap/pyproject.toml
 # Ensure our GEOS library will be used.
 export GEOS_DIR=${PREFIX}
 
+# Override Python if building noarch on a cross-compiling environment.
+case ${PKG_NAME} in
+    basemap-data|basemap-data-hires)
+        if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
+            ln -sf ${PREFIX}/bin/python ${BUILD_PREFIX}/bin/python
+        fi
+    ;;
+esac
+
 # Proceed based on the `basemap-split` subpackages.
 case ${PKG_NAME} in
     basemap|basemap-data|basemap-data-hires)
-        if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ]; then
-            rm ${BUILD_PREFIX}/bin/python
-            ln -sf ${PREFIX}/bin/python ${BUILD_PREFIX}/bin/python
-        fi
         pkgdir="packages/$(echo ${PKG_NAME} | tr - _)"
         ${PYTHON} -m pip install -vvv --ignore-installed --no-deps ${pkgdir}
         ;;
